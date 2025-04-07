3cf395d23e3635ad59a4fac68c0fd975019ea867
Taylor Noble <Zach.Noble@ey.com>
2025-04-07 11:17:44 -0400

    added access denied page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import AccessDenied from './AccessDenied.tsx'
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+    {
+        path: "/access-denied",
+        element: <AccessDenied />,
+    },
+])
+
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>,
 )
