1a5ec7df720bce3555ec09334d4e64708288ae1d
Taylor Noble <Zach.Noble@ey.com>
2025-04-07 11:38:41 -0400

    remove hyphen from url

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ const router = createBrowserRouter([
         element: <App />,
     },
     {
-        path: "/access-denied",
+        path: "/accessdenied",
         element: <AccessDenied />,
     },
 ])
